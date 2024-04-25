2262fa599f0d7f9df5abf46a1d06fe74b9e4ad48
alicemlima <alicemariaferreiradelima@gmail.com>
2024-04-24 23:00:06 -0300

    Implemente uma requisição para que campos específicos do cliente possam ser atualizados (Patch).

--- spring-rest-api-app/src/main/java/com/jeanlima/springrestapiapp/rest/controllers/ClienteController.java
+++ spring-rest-api-app/src/main/java/com/jeanlima/springrestapiapp/rest/controllers/ClienteController.java
@@ -5,15 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.jeanlima.springrestapiapp.model.Cliente;
@@ -80,6 +72,20 @@ public class ClienteController {
 
         Example example = Example.of(filtro, matcher);
         return clientes.findAll(example);
+    }
+
+    @PatchMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateNome( @PathVariable Integer id,
+                       @RequestParam String nome ){
+        clientes
+                .findById(id)
+                .map( clienteExistente -> {
+                    clienteExistente.setNome(nome);
+                    clientes.save(clienteExistente);
+                    return clienteExistente;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Cliente não encontrado") );
     }
 
 }
